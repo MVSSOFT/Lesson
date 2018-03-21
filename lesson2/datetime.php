cde9335e36583aceea61747a327e73b82d9b9b7b
Vach <vmedoyan@gmail.com>
2018-03-21 17:37:47 +0400

    mktime

--- lesson2/datetime.php
+++ lesson2/datetime.php
@@ -1,8 +1,11 @@
 <?php
 echo date("Y/d/m");
 echo "<br>";
-$date = mktime(11, 14, 54, 8, 12, 2014);
-echo date("Y/d/M h:m:s",$date);
+
+$date = mktime(23, 11, 25, 8, 12, 2014);
+echo $date;
+echo "<br>";
+echo date("Y-m-d H:i:s",$date);
 echo "<br>";
 $dateFromString = strtotime("2015/12/01");
 echo $dateFromString;
